8928f1a270488721fb43ea995570ee00039db3f1
Pst-LET <ligomter@gmail.com>
2022-06-30 16:59:41 -0700

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints output with putchar function
+ *
+ * Return: 1
+ */
+int main(void)
+{
+char str = 'and that piece of art is useful\" - Dora Korpar,';
+char num = '2015-10-19';
+putchar(str, num);
+ return (-1);
+}
