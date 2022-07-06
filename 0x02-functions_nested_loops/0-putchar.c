0c926a3db9f656e1d162c0ad97a1ea0a3b80a5a2
Pst-LET <ligomter@gmail.com>
2022-07-06 03:30:29 -0700

    Print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- * print_putchar - Prints "_putchar"
- *
- * Return: 0
- */
+* print_putchar - Prints "_putchar"
+*
+* Return: 0
+*/
 void print_putchar(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 }
