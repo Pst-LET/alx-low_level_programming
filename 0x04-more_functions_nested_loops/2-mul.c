1a8e61870855449f33c66c57ab43cafabc7d8ce2
Pst-LET <ligomter@gmail.com>
2022-07-07 07:45:34 -0700

    integer multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: first integer number
+ * @b: second integer number
+ *
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
