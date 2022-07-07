c6868d50b79505ca86d2d2c24b0ab52b74ea592f
Pst-LET <ligomter@gmail.com>
2022-07-07 09:12:40 -0700

    Print more

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - print 0 - 9 without 2 and 4
+ *
+ * Return: nothing
+ */
+void print_most_numbers(void)
+{
+	int i;
+	i = '0';
+for (i = '0'; i <= '9'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
