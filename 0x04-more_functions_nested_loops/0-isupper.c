0994f1329b0699f183ae3a48f89dcc860e914613
Pst-LET <ligomter@gmail.com>
2022-07-07 06:53:47 -0700

    is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,19 @@
 #include "main.h"
 /**
  * _isupper - checks for uppercase character.
+ * @c: character to be checked
  *
  * Return: 1 if c is uppercase
  * 0 otherwise.
  */
 int _isupper(int c)
 {
-if (c >= '0' && c <= '9')
-	return (1);
+if (c <= 'A')
+{
+return (1);
+}
+else
+{
 return (0);
+}
 }
