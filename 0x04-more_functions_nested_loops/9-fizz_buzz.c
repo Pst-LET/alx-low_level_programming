a88569cfbe219c9324bf1ab6cc1b0404db6de8de
Pst-LET <ligomter@gmail.com>
2022-07-08 13:57:18 -0700

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,11 +9,11 @@ int main()
 int i;
 for (i = 1; i <= 100; i++)
 {
-if ((i%15) == 0)
+if (i%15 == 0)
 {
 printf("FizzBuzz\t");
 }
-else if ((i%3) == 0) 
+else if ((i%3) == 0)
 {
 printf("Fizz\t");
 }
