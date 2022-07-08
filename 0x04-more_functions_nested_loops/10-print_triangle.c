4c67c06f70ee448fad5ca8dae8410c9d4f02086a
Pst-LET <ligomter@gmail.com>
2022-07-08 15:37:10 -0700

    Triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_triangle - prints triangle with #
+ * @size: size of triangle
+ * Return: nothing
+ */
+void print_triangle(int size)
+{
+int i;
+int j;
+int k;
+
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 1; i <= size; i++)
+{
+for (j = i; j < size; j++)
+{
+_putchar(' ');
+}
+for (k = 1; k <= i; k++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
