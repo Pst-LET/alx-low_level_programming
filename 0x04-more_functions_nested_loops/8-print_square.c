36c63ea2240cfe55ad8c509757601c88c61f48eb
Pst-LET <ligomter@gmail.com>
2022-07-08 13:51:07 -0700

    Fizz Buzz

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,17 +6,20 @@
  */
 void print_square(int size)
 {
-int i, j;
+int i;
+int j;
+
 if (size <= 0)
 {
 _putchar('\n');
 }
-else{
+else
+{
 for (i = 0; i < size; i++)
 {
 for (j = 0; j < size; j++)
 {
-_putchar('#');	
+_putchar('#');
 }
 _putchar('\n');
 }
