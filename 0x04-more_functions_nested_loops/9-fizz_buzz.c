4de9226fbf9a23bc0abdb8bb962561f3422f9a99
Pst-LET <ligomter@gmail.com>
2022-07-08 14:23:41 -0700

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,22 +4,22 @@
  *
  * Return: 0
  */
-int main()
+int main(void)
 {
 int i;
 for (i = 1; i <= 100; i++)
 {
-if (i%15 == 0)
+if (i % 15 == 0)
 {
 printf("FizzBuzz\t");
 }
-else if ((i%3) == 0)
+else if ((i % 3) == 0)
 {
 printf("Fizz\t");
 }
-else if ((i%5) == 0)
+else if ((i % 5) == 0)
 {
-printf("Buzz\t");
+printf("Buzz \n");
 }
 else
 {
