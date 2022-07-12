8e0514d8e5208183c719eb4834cc5408a98940ec
Pst-LET <ligomter@gmail.com>
2022-07-12 14:01:32 +0000

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _atoi - converts a string to an integer
+ * @s: pointer
+ * Return: 0 for no numbers in the string
+ */
+int _atoi(char *s)
+{
+	char tmp;
+
+	tmp = atoi(s);
+	if(*s == '\0')
+		return (0);
+	else 
+		return (tmp);
+}
