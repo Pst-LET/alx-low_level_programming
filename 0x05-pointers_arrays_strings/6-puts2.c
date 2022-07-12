36df389eea22b712b3ec976efc27a70c1983fb35
Pst-LET <ligomter@gmail.com>
2022-07-12 10:21:07 +0000

    Print characters

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts2 - prints every other character of the string starting with the first.character
+ * @str: pointer 
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int i;
+	int len;
+
+	len = strlen(str);
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
