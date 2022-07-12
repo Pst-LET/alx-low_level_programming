d8186a741d882e8324bef1b54c418380da3557ce
Pst-LET <ligomter@gmail.com>
2022-07-12 09:34:16 +0000

    Print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_rev - prints a string in reverse
+ * @s: string to be printed
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int n;
+	int i;
+
+	n = 0;
+	while ( s[n] != '\0')
+	{
+		n++;
+	}
+	for (i = n-1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
