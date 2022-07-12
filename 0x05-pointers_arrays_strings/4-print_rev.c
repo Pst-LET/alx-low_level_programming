1d6bc2dc0339a1d7a95a2c13e2b540dde23c0920
Pst-LET <ligomter@gmail.com>
2022-07-12 09:36:14 +0000

    Print reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,11 +11,11 @@ void print_rev(char *s)
 	int i;
 
 	n = 0;
-	while ( s[n] != '\0')
+	while (s[n] != '\0')
 	{
 		n++;
 	}
-	for (i = n-1; i >= 0; i--)
+	for (i = n - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
