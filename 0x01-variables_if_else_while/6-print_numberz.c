d512188d9cafa3db5130a6a70157ddcf62591251
Pst-LET <ligomter@gmail.com>
2022-07-14 16:06:41 +0000

    Print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints numbers in base 10
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num <= 57; ++num)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
