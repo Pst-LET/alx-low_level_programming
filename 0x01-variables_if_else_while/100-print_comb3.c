57a48aef2ac8978f64c47e7f60f62c7566876580
Pst-LET <ligomter@gmail.com>
2022-07-14 20:03:00 +0000

    combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - prints possible different combinations of two digits
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+	int i;
+
+	for (num = 48; num <= 57; num++)
+	{
+		for (i = 49; i <= 57; i++)
+		{
+			if (i > num)
+			{
+				putchar(num);
+				putchar(i);
+			if (num != 56 || i != 57)
+			{
+				putchar(',');
+				putchar(32);
+			}
+		}
+		}
+	}
+	return (0);
+}
