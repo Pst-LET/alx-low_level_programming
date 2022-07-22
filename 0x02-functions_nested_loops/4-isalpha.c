0405782925ba8a308da60c5d0be38dcab1bf3fd8
Pst-LET <ligomter@gmail.com>
2022-07-22 10:52:35 +0100

    Static Libraries

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: character
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+int _isalpha(int c)
+{
+
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * Return: absolute value
+ */
+int _abs(int)
+{
+
+}
