85f1f5970c3c4fe84fa129401d4297195b57d8a5
Pst-LET <ligomter@gmail.com>
2022-07-22 14:37:13 +0100

    args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments passed to it
+ * @argc: count
+ * @argv: array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int k;
+
+	for (k = 0; k < argc; k++)
+	{
+		printf("%s\n", argv[k]);
+	}
+	return (0);
+}
