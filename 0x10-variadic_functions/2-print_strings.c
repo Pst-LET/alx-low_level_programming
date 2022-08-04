0f52092daed7484ff4b4e81cec50d02a6208edfa
Pst-LET <ligomter@gmail.com>
2022-08-05 00:26:47 +0100

    print

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,7 +13,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *str;
 
 	va_start(ap, n);
-	for (i = 0; i < n; i++)
+	for (; i < n; i++)
 	{
 		if (separator == NULL)
 			return;
