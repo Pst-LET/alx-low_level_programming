a59087decf4b4b94cd8276b8491baa4eabbd88da
Pst-LET <ligomter@gmail.com>
2022-08-05 00:34:09 +0100

    print

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,17 +9,17 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
-	unsigned int i;
+	unsigned int i = 0;
 	int num;
 
 	va_start(ap, n);
-	for (i = 0; i < n; i++)
+	for (; i < n; i++)
 	{
 		num = va_arg(ap, int);
 		printf("%d", num);
 
 		if (separator && i < n - 1)
-			printf("%c ", *separator);
+			printf("%s", separator);
 	}
 	printf("\n");
 	va_end(ap);
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,22 +9,19 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
-	unsigned int i;
+	unsigned int i = 0;
 	char *str;
 
 	va_start(ap, n);
 	for (; i < n; i++)
 	{
-		if (separator == NULL)
-			return;
 		str = va_arg(ap, char *);
 		if (str == NULL)
 			printf("(nil)");
 		else
 			printf("%s", str);
-
-	if (i < n - 1)
-		printf("%s", separator);
+		if (separator && i < n - 1)
+			printf("%s", separator);
 	}
 	printf("\n");
 	va_end(ap);
