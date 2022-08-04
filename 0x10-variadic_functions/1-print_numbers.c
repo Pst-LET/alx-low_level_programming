a60a468cb35c4443c3bbcd6fcc1707474ad396c0
Pst-LET <ligomter@gmail.com>
2022-08-04 23:09:24 +0100

    print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,10 +12,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(ap, n);
+	
+	if (separator == NULL)
+		return;
 	for (i = 0; i < n; i++)
 	{
-		if (separator == NULL)
-			return;
 		printf("%d", va_arg(ap, int));
 		if (i < n - 1)
 			printf("%c ", *separator);
