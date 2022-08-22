a206b475a230a5cb96f646142f2858808f97ac55
Pst-LET <ligomter@gmail.com>
2022-08-22 18:57:52 +0100

    Read text file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * read_text - Reads a text file and prints the output.
+ * @filename: Name of the file
+ * @letters: Number of characters to read and print
+ *
+ * Return: The number of characters read and printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t readlen, writelen;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+	{
+		close (fd);
+		return (0);
+	}
+	readlen = read(fd, buf, letters);
+	close(fd);
+	if (readlen == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	writelen = write(STDIN_FILENO, buf, readlen);
+	free(buf);
+	if (readlen != writelen)
+		return (0);
+	return (writelen);
+		
+}
